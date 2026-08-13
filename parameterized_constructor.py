89e086802129e19ff59fce34244cdd6dd0c2d01e
UlfathAraIslam <mouliajsahid@gmail.com>
2026-08-14 00:13:10 +0900

    updated parameterized constractor

--- parameterized_constructor.py
+++ parameterized_constructor.py
@@ -1,9 +1,10 @@
 class Student:
     def __init__(self,name,Id):
-        self.name = name
-        self.id = Id
+        self.name = name        #instance variable
+        self.id = Id            #instance variable
 
 s1 = Student("Bob",11)
 s2 = Student("Carol",22)
-
+s1.id = 33
 print(s1.name)
+print(s1.id)
