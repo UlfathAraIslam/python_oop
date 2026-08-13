5617e2b281fab40dc1784ac6a15edc5b54ecc6e2
UlfathAraIslam <mouliajsahid@gmail.com>
2026-08-13 23:31:32 +0900

    default constructor and object creation

---
+++ default_constructor.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self):
+        print("self",self)
+        print("A student object is created")
+
+# variable = class_name()
+s1 = Student()
+s2 = Student()
+print(s1)
